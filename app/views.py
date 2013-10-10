d8a0d258f7b84089ee31bfbd1f264aa264e6d6ec
Renan Cakirerk <renan@redbeacon.com>
2013-10-10 00:33:21 -0700

    Cosmetic

--- app/views.py
+++ app/views.py
@@ -4,12 +4,15 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
+
 def login(request):
     return render_to_response('login.html', {'user': request.user}, RequestContext(request))
+
 
 @login_required()
 def index(request):
     return render_to_response('index.html', RequestContext(request))
+
 
 @login_required()
 def home(request):
