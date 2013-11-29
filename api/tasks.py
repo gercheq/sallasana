9fe4100f8966df132fa1770af785adad2ed3dfb4
Renan Cakirerk <renan@redbeacon.com>
2013-11-29 14:15:48 -0800

    Add Likes to profile \o/

--- api/tasks.py
+++ api/tasks.py
@@ -6,12 +6,63 @@ from celery import task
 from mongoengine import DoesNotExist
 
 from app.models import FBProfile
+from app.models import Like
 
 
 @task()
 def update_fb_profile(fb_profile, access_token):
     graph_api = facebook.GraphAPI(access_token)
 
+    """
+    LIKE OPERATIONS
+    """
+    likes_up_to_date = False
+
+    # Get likes
+    likes = graph_api.get_object('me/likes', limit=1000000)['data']
+
+    # Creating a dictionary where the key is l_id and value is l_name
+    like_id_to_name_dict = dict()
+
+    for like in likes:
+        like_id_to_name_dict[like.get('id')] = like.get('name')
+
+    # Saving likes
+    if not fb_profile.likes == like_id_to_name_dict:
+        likes_up_to_date = False
+        print "Likes are not up to date. I'll update them now."
+    else:
+        likes_up_to_date = True
+        print "Likes are up to date"
+
+    if not likes_up_to_date:
+        # Saving likes to FBProfile
+        fb_profile.likes = like_id_to_name_dict
+        fb_profile.save()
+
+        for like in likes:
+            try:
+                # is like in the db?
+                fb_like = Like.objects.get(l_id=like['id'])
+
+                print "Like %s already in db, skipping" % like['id']
+
+            except DoesNotExist:
+                # Creating a Like object
+
+                like_context = graph_api.get_object(like['id'])
+
+                print "adding like %s to db" % like['id']
+
+                like_context['l_id'] = like['id']
+                del like_context['id']
+
+                new_like = Like(**like_context)
+                new_like.save()
+
+    """
+    FRIENDS OPERATIONS
+    """
     friends_up_to_date = False
 
     # Get friends
@@ -39,22 +90,23 @@ def update_fb_profile(fb_profile, access_token):
     fb_profile.picture = user_picture
     fb_profile.save(safe=True)
 
-    # Creating a Facebook Profile for each friend
-    # skips if already exists
     if not friends_up_to_date:
+        # Saving friend dict to profile
         fb_profile.friends = friend_id_to_name_dict
         fb_profile.save()
 
+        # Creating a Facebook Profile for each friend
+        # skips if already exists
         for profile in friends:
             try:
                 # is my friend in the db?
-                fb_user = FBProfile.objects.get(fb_id=profile['id'])
+                friend_profile = FBProfile.objects.get(fb_id=profile['id'])
 
                 # Adding self as friend
-                fb_user.friends[fb_profile['fb_id']] = fb_profile['name']
-                fb_user.save()
+                friend_profile.friends[fb_profile['fb_id']] = fb_profile['name']
+                friend_profile.save()
 
-                print "%s already in db, skipping" % profile['id']
+                print "Profile %s already in db, skipping" % profile['id']
 
             except DoesNotExist:
                 """
@@ -67,7 +119,7 @@ def update_fb_profile(fb_profile, access_token):
                 friend_profile = graph_api.get_object(profile['id'])
                 friend_picture = graph_api.get_object(profile['id'], fields='picture.height(1024).width(1024)')
 
-                print "adding %s to db" % profile['id']
+                print "adding profile %s to db" % profile['id']
 
                 # Replacing id with fb_id so it doesn't get mixed with MongoDB id
                 friend_profile['fb_id'] = profile['id']
@@ -80,7 +132,7 @@ def update_fb_profile(fb_profile, access_token):
                 friend_profile['friends'] = {}
                 friend_profile['friends'][fb_profile['fb_id']] = fb_profile['name']
 
-                generated_profile = FBProfile(**friend_profile)
-                generated_profile.save()
+                new_profile = FBProfile(**friend_profile)
+                new_profile.save()
 
     return True
